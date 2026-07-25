5ead22501b7d44578cfe3b5acc403460ac268b4a
gaurang3106 <gaurangkumawat026@gmail.com>
2026-07-25 09:22:15 +0530

    Fix exception syntax in send_mail

--- canteen/accounts/views.py
+++ canteen/accounts/views.py
@@ -212,9 +212,9 @@ def send_verification_email(request, user):
         )
         return 1
     except Exception as e:
-    import traceback
-    traceback.print_exc()
-    return 0
+        import traceback
+        traceback.print_exc()
+        return 0
 
 def customer_register(request):
     form = CustomerSignupForm(request.POST or None)
